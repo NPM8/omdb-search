1425f27fafc0b8c6b5c236a5be18a6a519dc79ae
Dawid NPM8 Malysa <nnnp.dawid@gmail.com>
2021-01-25 16:09:58 +0100

    Add fetch mock to tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import FetchMock from 'jest-fetch-mock';
+
+FetchMock.enableMocks();
+
