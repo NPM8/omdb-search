2bd980e9c52d94ebb7acde615ce2a02eada77a2b
Dawid NPM8 Malysa <nnnp.dawid@gmail.com>
2021-01-26 11:53:00 +0100

    Fix typo in test expects

--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -3,11 +3,11 @@ import {encodeQuery} from './index'
 describe('Utils', () => {
     describe('encodeQuery', () => {
         it('changes \' \' to \'+\'', () => {
-            expect(encodeQuery('test t')).toBe('test+te')
+            expect(encodeQuery('test t')).toBe('test+t')
         });
 
         it('changes \'&\' to \'%26\'', () => {
-            expect(encodeQuery('test&t')).toBe('test%26te')
+            expect(encodeQuery('test&t')).toBe('test%26t')
         });
     })
 })
