ec568d436405ab48575998cb9c9f023dc8d473ea
Dawid NPM8 Malysa <nnnp.dawid@gmail.com>
2021-01-26 10:04:45 +0100

    Change protocol for security on gh-pages

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,13 @@
 import { SearchResult, Response } from './types';
 
 export const searchRequest: (query: string, page: number) => Promise<SearchResult> = async (query, page) => {
-    const data = await fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${query}&page=${page.toString()}`);
+    const data = await fetch(`https://www.omdbapi.com/?apikey=${
+        process.env.REACT_APP_API_KEY
+    }&s=${
+        encodeURI(query)
+    }&page=${
+        page.toString()
+    }`);
     const json: Response = await data.json();
 
     if (json.Response === "True") {
